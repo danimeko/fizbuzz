6741d48017544954d9a22e8eddc33d1447c176e2
Daniel <daniel.mengistu@hotmail.com>
2019-01-09 14:14:35 +0200

    cleaning the code

--- src/main/java/FiizBuzz/Controller.java
+++ src/main/java/FiizBuzz/Controller.java
@@ -8,24 +8,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    private static final String template = "Output, %s!";
+
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/fiizbuzz")
-    public Engine engine(@RequestParam(value="name", defaultValue="World") String name) {
+    public Engine engine(@RequestParam(value="inputNumber") String inputNumber) {
 
-        String[] parts = name.split(",");
+        //split the input string separator comma and put them in the string of array
+        String[] parts = inputNumber.split(",");
+
         String str ="";
+        //loop the string of array and determine if it is fiiz or booz or fiizbozz and concatinate it to outout srting
         for(int i = 0; i < parts.length ; i++){
-            System.out.println(parts[i]);
+           //
             int result = Integer.parseInt(parts[i]);
 
-            if(result % 3 == 0 ) {
-                str  = str + "Fiiz ";
+            if(result % 3 == 0 && result % 5 == 0) {
+                str  = str + "FiizBuzz ";
             }else if(result % 5 == 0 ){
                 str  = str + "Buzz ";
-            }else if(result % 15 == 0){
-                str  = str + "FiizBuzz ";
+            }else if(result % 3 == 0){
+                str  = str + "Fiiz ";
             }else
                 str  = str + parts[i] + " ";
         }
@@ -33,6 +36,8 @@ public class Controller {
 
 
         return new Engine(counter.incrementAndGet(),
-                String.format(template, str));
+                            inputNumber,
+                            str
+        );
     }
 }
--- src/main/java/FiizBuzz/Engine.java
+++ src/main/java/FiizBuzz/Engine.java
@@ -3,18 +3,25 @@ package FiizBuzz;
 public class Engine {
 
     private final long id;
-    private final String content;
+    private final String output;
+    private final String input;
 
-    public Engine(long id, String content) {
+    public Engine(long id,String input,String output) {
         this.id = id;
-        this.content = content;
+        this.input = input;
+        this.output = output;
+
     }
 
     public long getId() {
         return id;
     }
 
-    public String getContent() {
-        return content;
+    public String getOutput() {
+        return output;
+    }
+
+    public String getInput() {
+        return input;
     }
 }
