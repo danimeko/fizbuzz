1c6d58578a63106539c953e84fc6d859537d441f
Daniel <daniel.mengistu@hotmail.com>
2019-01-09 11:39:48 +0200

    bug fix

--- src/main/java/FiizBuzz/Controller.java
+++ src/main/java/FiizBuzz/Controller.java
@@ -8,12 +8,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Output, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/fiizbuzz")
     public Engine engine(@RequestParam(value="name", defaultValue="World") String name) {
+
+        String[] parts = name.split(",");
+        String str ="";
+        for(int i = 0; i < parts.length ; i++){
+            System.out.println(parts[i]);
+            int result = Integer.parseInt(parts[i]);
+
+            if(result % 3 == 0 ) {
+                str  = str + "Fiiz ";
+            }else if(result % 5 == 0 ){
+                str  = str + "Buzz ";
+            }else if(result % 15 == 0){
+                str  = str + "FiizBuzz ";
+            }else
+                str  = str + parts[i] + " ";
+        }
+
+
+
         return new Engine(counter.incrementAndGet(),
-                String.format(template, name));
+                String.format(template, str));
     }
 }
